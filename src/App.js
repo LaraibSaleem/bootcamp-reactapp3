833e84ab551dc4d04ff193deed533fe2fcd2fba5
Laraib_Saleem <laraibsaleem98@gmail.com>
2020-06-18 16:24:13 +0500

    Full Counter and Theme Changer App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Message} from './Message.js';
 
 function App() {
+
+  let [count, setCount] = useState(1);
+  let [isMorning, setMorning] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`box ${isMorning? 'dayLight' : ''}`}>
+
+      <h1> Good {isMorning ? 'Morning' : 'Night'} </h1>
+
+      <Message counter={count}/>
+
+      <button onClick={ () => setCount(++count) }> Update Counter </button>
+
+      <button onClick={ () => setMorning(!isMorning) }> Change from Night to Day </button>
     </div>
   );
 }
